60ea5e64a4c04ed1cb66fdea7aa33823302e2183
Valter Karlsson <valterkarlsson2002@gmail.com>
2025-11-16 23:59:46 +0100

    Pascal's triangle done

---
+++ Assignment1/Lecture1/pascal.py
@@ -0,0 +1,17 @@
+def pascal_line(n):
+    first_line = [1]
+    return pascal_rec(first_line, n)
+
+
+def pascal_rec(lst, n):
+    if n == 0:
+        return lst
+    new_lst = [1]
+    for i in range(0, len(lst)-1):
+        new_lst.append(lst[i] + lst[i+1])
+    new_lst.append(1)
+    return pascal_rec(new_lst, n-1)
+
+
+user_input = int(input('Enter n: '))
+print(pascal_line(user_input))
