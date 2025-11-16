76e9eb8d869f64b11f84e7bb3e7962e9273331b4
Valter Karlsson <valterkarlsson2002@gmail.com>
2025-11-16 22:36:56 +0100

    Init

--- Assignment1/Lecture1/palindrome.py
+++ Assignment1/Lecture1/palindrome.py
@@ -1,1 +1,28 @@
-print('hello world!')
+def check_palindrome(s):
+    if s.strip() == '' or len(s) == 1:
+        return True
+    first = s[0]
+    last = s[len(s)-1]
+    s = s[1:-1]
+    return is_palindrome(s, first, last)
+
+
+def is_palindrome(s, first, last):
+    if first == last:
+        while len(s) > 0:
+            first = s[0]
+            last = s[len(s)-1]
+            if first == last:
+                s = s[1:-1]
+            else:
+                return False
+    else:
+        return False
+    return True
+
+
+user_input = str(input('Please enter a string: '))
+if check_palindrome(user_input):
+    print(f'{user_input} is a palindrome.')
+else:
+    print(f'{user_input} is not a palindrome.')
