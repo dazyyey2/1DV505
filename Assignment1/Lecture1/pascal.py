00c873cbc322e958cffa5b0c8f20adf709f8e34e
Valter Karlsson <valterkarlsson2002@gmail.com>
2025-11-17 01:29:05 +0100

    change

--- Assignment1/Lecture1/pascal.py
+++ Assignment1/Lecture1/pascal.py
@@ -6,11 +6,11 @@ def pascal_line(n):
 def pascal_rec(lst, n):
     if n == 0:
         return lst
-    new_lst = [1]
+    new_line = [1]
     for i in range(0, len(lst)-1):
-        new_lst.append(lst[i] + lst[i+1])
-    new_lst.append(1)
-    return pascal_rec(new_lst, n-1)
+        new_line.append(lst[i] + lst[i+1])
+    new_line.append(1)
+    return pascal_rec(new_line, n-1)
 
 
 user_input = int(input('Enter n: '))
